93a9fc9cdee955c05cd8e07bfdf8519787b035a7
elainezchen <ezc3@georgetown.edu>
2020-01-27 18:13:16 -0500

    Update game.py

--- game.py
+++ game.py
@@ -1,12 +1,19 @@
 # game.py
 
+import random
+
 print("Rock, Paper, Scissors, Shoot!")
 
 # prompts user to input a RPS option
 x = input("Please input either 'Rock', 'Paper', or 'Scissors': ")
 
-if(x.lower() != "rock" or x.lower() != "paper" or x.lower() != "scissors"):
+if(x.lower() != "rock" and x.lower() != "paper" and x.lower() != "scissors"):
     print("That was not an accepted input. Please input a valid option next time.")
     exit()
 else:
-    print(x)
+    x = x.lower()
+
+compchoice = ["rock", "paper", "scissors"]
+y = random.choice(compchoice)
+
+
