a08458fa3d6dde5361f33d6cb779e3e5c8516b0a
elainezchen <ezc3@georgetown.edu>
2020-01-27 17:37:55 -0500

    game.py

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # rock-paper-scissors
+
+# set up (first time) conda create -n my-game-env python=3.7
+# conda activate my-game-env
+
+# python game.py
--- game.py
+++ game.py
@@ -1,3 +1,12 @@
 # game.py
 
 print("Rock, Paper, Scissors, Shoot!")
+
+# prompts user to input a RPS option
+x = input("Please input either 'Rock', 'Paper', or 'Scissors': ")
+
+if(x.lower() != "rock" or x.lower() != "paper" or x.lower() != "scissors"):
+    print("That was not an accepted input. Please input a valid option next time.")
+    exit()
+else:
+    print(x)
